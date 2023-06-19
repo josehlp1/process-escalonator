b60b38993bda5186624f9eeae4fee4c1a4e7a78d
josehlp <jose.l.pellet@gmail.com>
2023-06-18 21:12:20 -0300

    disk features add

--- disk_emulator.py
+++ disk_emulator.py
@@ -10,11 +10,21 @@ class DiskEmulator:
         operation = random.choice(['leitura', 'escrita'])
 
         if operation == 'leitura':
-            self.available_space += file_size
-            return f"Leitura de arquivo de tamanho {file_size} realizada. Espaço disponível no disco: {self.available_space}/{self.disk_space}"
+            if self.available_space + file_size > self.disk_space:
+                return f"Erro: Não há espaço suficiente no disco para ler um arquivo de tamanho {file_size}. Espaço disponível: {self.available_space}/{self.disk_space}"
+            else:
+                self.available_space += file_size
+                return f"Leitura de arquivo de tamanho {file_size} realizada. Espaço disponível no disco: {self.available_space}/{self.disk_space}"
         else:
             if file_size <= self.available_space:
                 self.available_space -= file_size
                 return f"Escrita de arquivo de tamanho {file_size} realizada. Espaço disponível no disco: {self.available_space}/{self.disk_space}"
             else:
                 return f"Não há espaço suficiente no disco para escrever um arquivo de tamanho {file_size}. Espaço disponível: {self.available_space}/{self.disk_space}"
+
+    def get_available_space(self):
+        return self.available_space
+
+    def clean_disk(self):
+        self.available_space = self.disk_space
+        return "Disco limpo. Espaço disponível: {}/{}".format(self.available_space, self.disk_space)
--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from cpu import CPU
 import random
 
 manager = ProcessManager()
-disk_emulator = DiskEmulator(1000)
+disk_emulator = DiskEmulator(5)
 mmu = MMU(256)
 cpu = CPU(8)
+waiting_queue = []
 
 def create_process():
     try:
@@ -20,12 +21,15 @@ def create_process():
     process_id = manager.create_process(priority)
     listbox.insert(tk.END, f'Processo {process_id} de prioridade {priority} criado.')
 
+
 def execute_process():
     process = manager.execute_process()
     if process:
-        file_size = random.randint(1, disk_emulator.disk_space)  # Valor aleatório dentro do espaço total do disco
+        file_size = random.randint(1, disk_emulator.disk_space)
         if file_size > disk_emulator.available_space:
-            listbox.insert(tk.END, f'Erro: Processo {process.id} de prioridade {process.priority} não pode ser executado. Tamanho do arquivo ({file_size}) excede o espaço disponível no disco ({disk_emulator.available_space}).')
+            listbox.insert(tk.END,
+                           f'Erro: Processo {process.id} de prioridade {process.priority} não pode ser executado. Tamanho do arquivo ({file_size}) excede o espaço disponível no disco ({disk_emulator.available_space}).')
+            waiting_queue.append(process)
         else:
             process.use_disk(disk_emulator, file_size)
             process.use_mmu(mmu, random.randint(0, mmu.memory_capacity))
@@ -40,13 +44,17 @@ def execute_process():
         listbox.insert(tk.END, '')
         listbox.insert(tk.END, 'Nenhum processo na fila para execução.')
 
+    execute_waiting_processes()  # Process waiting queue
+
 
 def execute_all_processes():
     process = manager.execute_process()
     while process:
-        file_size = random.randint(1, disk_emulator.disk_space)  # Valor aleatório dentro do espaço total do disco
+        file_size = random.randint(1, disk_emulator.disk_space)
         if file_size > disk_emulator.available_space:
-            listbox.insert(tk.END, f'Erro: Processo {process.id} de prioridade {process.priority} não pode ser executado. Tamanho do arquivo ({file_size}) excede o espaço disponível no disco ({disk_emulator.available_space}).')
+            listbox.insert(tk.END,
+                           f'Erro: Processo {process.id} de prioridade {process.priority} não pode ser executado. Tamanho do arquivo ({file_size}) excede o espaço disponível no disco ({disk_emulator.available_space}).')
+            waiting_queue.append(process)
         else:
             process.use_disk(disk_emulator, file_size)
             process.use_mmu(mmu, random.randint(0, mmu.memory_capacity))
@@ -60,12 +68,45 @@ def execute_all_processes():
 
         process = manager.execute_process()
 
+    execute_waiting_processes()
+
+def get_available_space():
+    space_label['text'] = f"Espaço disponível no disco: {disk_emulator.get_available_space()}/{disk_emulator.disk_space}"
+
+
+def clean_disk():
+    disk_emulator.clean_disk()
+    space_label[
+        'text'] = f"Espaço disponível no disco: {disk_emulator.get_available_space()}/{disk_emulator.disk_space}"
+    listbox.insert(tk.END, "Disco limpo. Todo o espaço está disponível novamente.")
+    execute_waiting_processes()
+
+def execute_waiting_processes():
+    while waiting_queue:
+        process = waiting_queue.pop(0)
+        file_size = random.randint(1, disk_emulator.disk_space)
+        if file_size > disk_emulator.available_space:
+            listbox.insert(tk.END,
+                           f'Erro: Processo {process.id} de prioridade {process.priority} não pode ser executado. Tamanho do arquivo ({file_size}) excede o espaço disponível no disco ({disk_emulator.available_space}).')
+            waiting_queue.append(process)
+        else:
+            process.use_disk(disk_emulator, file_size)
+            process.use_mmu(mmu, random.randint(0, mmu.memory_capacity))
+            process.use_cpu(cpu)
+            listbox.insert(tk.END, '')
+            listbox.insert(tk.END, f'Processo {process.id} de prioridade {process.priority} iniciado.')
+            listbox.insert(tk.END, f'Processo {process.id} de prioridade {process.priority} concluído.')
+            listbox.insert(tk.END, f'Métrica de Disco: {process.disk_usage}')
+            listbox.insert(tk.END, f'Métrica de MMU: {process.mmu_usage}')
+            listbox.insert(tk.END, f'Métrica de CPU: {process.cpu_usage}')
+
+    space_label['text'] = f"Espaço disponível no disco: {disk_emulator.get_available_space()}/{disk_emulator.disk_space}"
 
 
 window = tk.Tk()
 window.title("Simulação de Gerenciamento de Processos")
 
-window.geometry("800x600")
+window.geometry("1000x800")
 
 priority_label = tk.Label(window, text="Prioridade:", font=('Arial', 14))
 priority_label.pack(pady=10)
@@ -83,6 +124,17 @@ execute_button.pack(pady=10)
 execute_all_button = tk.Button(window, text="Executar Todos", command=execute_all_processes, height=2, width=30,
                                font=('Arial', 14))
 execute_all_button.pack(pady=10)
+
+space_label = tk.Label(window, text="Espaço disponível no disco: {}/{}".format(disk_emulator.get_available_space(), disk_emulator.disk_space), font=('Arial', 14))
+space_label.pack(pady=10)
+
+get_space_button = tk.Button(window, text="Ver Espaço Disponível", command=get_available_space, height=2, width=30,
+                             font=('Arial', 14))
+get_space_button.pack(pady=10)
+
+clean_disk_button = tk.Button(window, text="Limpar Disco", command=clean_disk, height=2, width=30,
+                              font=('Arial', 14))
+clean_disk_button.pack(pady=10)
 
 listbox = tk.Listbox(window, width=100, height=20, font=('Arial', 14))
 listbox.pack(pady=10)
